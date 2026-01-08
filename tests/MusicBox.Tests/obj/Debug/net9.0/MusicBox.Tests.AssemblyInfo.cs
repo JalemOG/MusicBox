609e2d8b3cbbcbf25ccca3628c590e6ef2b429c2
JalemOG <janikhamilton1441@gmail.com>
2026-01-08 14:06:42 -0600

    feat: tests unitarios

--- tests/MusicBox.Tests/obj/Debug/net9.0/MusicBox.Tests.AssemblyInfo.cs
+++ tests/MusicBox.Tests/obj/Debug/net9.0/MusicBox.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicBox.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+954ba1b2700ee7469f5ee5c4b1568d98770e7e3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicBox.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicBox.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
